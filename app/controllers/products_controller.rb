95e58f52c700b867c7be5710d7be9e45c783e7fe
Subhash Kumar <jietss2011@gmail.com>
2016-09-20 17:33:21 +0530

    checkout completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,10 @@ class ProductsController < ApplicationController
   def show
     @product = Shoppe::Product.root.find_by_permalink(params[:permalink])
   end
+
+  def buy
+     @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+     current_order.order_items.add_item(@product, 1)
+     redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
 
   get "product/:permalink", to: "products#show", as: "product"
 post "product/:permalink", to: "products#buy", as: "buy"
+#post "product/:permalink", to: "products#buy"
+get "basket", to: "orders#show"
+delete "basket", to: "orders#destroy"
+match "checkout", to: "orders#checkout", as: "checkout", via: [:get, :patch]
+match "checkout/pay", to: "orders#payment", as: "checkout_payment", via: [:get, :post]
+match "checkout/confirm", to: "orders#confirmation", as: "checkout_confirmation", via: [:get, :post]
+
 root to: "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
